5c34fb14ebafeff21727165bc3cdd56eafdfc4e2
gumoraes525 <gumenezes2019@googlemail.com>
2025-11-19 15:14:46 -0300

    Update aosp_vboxware.mk for LineageOS ICS

--- aosp_vboxware.mk
+++ aosp_vboxware.mk
@@ -1,33 +1,32 @@
 #
-# Copyright (C) 2024 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
+# Android 4.0 / LineageOS ICS Product Definition
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+# Inherit from core/base products (most specific first)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from device
 $(call inherit-product, device/virt/vboxware/device.mk)
 
-$(call enforce-product-packages-exist,\
-    android.hardware.health@2.0-impl-default.recovery \
-    DeviceDiagnostics \
-    product_manifest.xml)
-
+# PRODUCT_NAME and device info
 PRODUCT_NAME := aosp_vboxware
 PRODUCT_DEVICE := vboxware
 PRODUCT_BRAND := vboxware
 PRODUCT_MANUFACTURER := vboxware
 PRODUCT_MODEL := vboxware
 
-PRODUCT_VENDOR_PROPERTIES += \
+# Vendor properties
+PRODUCT_VENDOR_PROPERTIES := \
     ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
     ro.soc.model=$(PRODUCT_DEVICE)
 
-# Workaround build fingerprint too long
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vboxware-eng 14 AP2A.240705.005 0 test-keys"
+# Build fingerprint workaround
+# ICS does not have PRODUCT_BUILD_PROP_OVERRIDES, so set BUILD_FINGERPRINT directly
+BUILD_FINGERPRINT := vboxware/vboxware/vboxware:4.0.4/ICS-eng/test-keys
 
-BUILD_FINGERPRINT := vboxware/vboxware/vboxware:14/AP2A.240705.005/0:eng/test-keys
+# Optional: packages to include (replace enforce-product-packages-exist)
+PRODUCT_PACKAGES := \
+    android.hardware.health@2.0-impl-default.recovery \
+    DeviceDiagnostics \
+    product_manifest.xml
